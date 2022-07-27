ad639916d24299118525aa6e5c95befa08d16d16
DariaIliuchenko <102221418+DariaIliuchenko@users.noreply.github.com>
2022-07-27 18:10:45 +0300

    домашка

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const { boxes, number, create, destroy } = {
+  boxes: document.querySelector("#boxes"),
+  number: document.querySelector(`[type="number"]`),
+  create: document.querySelector(`[data-create]`),
+ destroy: document.querySelector(`[data-destroy]`),
+}
+function destroyEl() { 
+
+}
